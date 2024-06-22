e6a4b7ee5eed20b82cccb3d7dd83a263d74bc796
Tatsuya Shiozawa <tatsuya.shiozawa@cocotone.jp>
2024-06-22 16:55:41 +0900

    format.

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,15 @@ function App() {
 
   return (
     <>
-      <div className="h-screen w-screen bg-gray-500 flex justify-center items-center">
-        <div className="h-96 w-96 border-4 border-cyan-100 flex justify-center items-center">
+      <div className="flex h-screen w-screen items-center justify-center bg-gray-500">
+        <div className="flex h-96 w-96 items-center justify-center border-4 border-cyan-100">
           <a href="https://vitejs.dev" target="_blank">
             <img src={viteLogo} alt="Vite logo" />
           </a>
           <a href="https://react.dev" target="_blank">
             <img src={reactLogo} alt="React logo" />
           </a>
-          <h1 className="text-3xl font-bold underline text-white">
+          <h1 className="text-3xl font-bold text-white underline">
             Vite + React
           </h1>
         </div>
